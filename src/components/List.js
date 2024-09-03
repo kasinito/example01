6d83d1ebf956cbe7cefc2100a12f5abdf1ec174d
kasi <a01046209769@gmail.com>
2024-09-03 20:26:36 +0900

    rewrite,delete

--- src/components/List.js
+++ src/components/List.js
@@ -3,30 +3,85 @@ import React, { useState } from "react";
 const List = () => {
   const [text, setText] = useState("");
   const [line, setLine] = useState([]);
+  const [idnum, setnum] = useState(1);
+  const [changeUp, setChangeUp] = useState(false);
+  const [fixNum, setFixNum] = useState("");
+  const [fixText, setFixText] = useState("");
 
-  const upload = () => {
-    setLine([...line, text]);
-    setText("");
+  const upload = (e) => {
+    e.preventDefault();
+    if (text !== "") {
+      const llet = {
+        id: idnum,
+        text: text,
+      };
+
+      setLine([...line, llet]);
+      setText("");
+      setnum(idnum + 1);
+    }
   };
   const changeable = (e) => {
-    setText(e.target.value);
+    if (changeUp) {
+      setFixText(e.target.value);
+    } else {
+      setText(e.target.value);
+    }
+  };
+  const deleteText = (idx) => {
+    setLine(line.filter((arr) => arr.id !== idx));
+  };
+  const reload = (e, idx) => {
+    console.log(idx);
+    e.preventDefault();
+
+    if (fixText !== "") {
+      const lLet = {
+        id: idx,
+        text: fixText,
+      };
+      setLine(line.map((arr) => (arr.id === idx ? lLet : arr)));
+      setFixText("");
+    }
+    setChangeUp(false);
+  };
+  const changeText = (idx) => {
+    setChangeUp(true);
+    setFixNum(idx);
   };
 
   return (
     <>
-      <input onChange={changeable} value={text}></input>
-      <button onClick={upload}>upload</button>
+      <form onSubmit={upload}>
+        <input
+          onChange={changeable}
+          value={text}
+          onKeyDown={changeable}
+        ></input>
+        <button>upload</button>
+      </form>
+
       <ul>
-        {line.map((content, idx) => {
-          return (
-            <form key={idx}>
-              <li>{content}</li>
-            </form>
-          );
+        {line.map((content) => {
+          if (changeUp && fixNum === content.id) {
+            return (
+              <form key={content.id} onSubmit={(e) => reload(e, content.id)}>
+                <input onChange={changeable} value={fixText}></input>
+                <button>confirm</button>
+              </form>
+            );
+          } else {
+            return (
+              <div key={content.id}>
+                <li>{content.text}</li>
+                <button onClick={() => deleteText(content.id)}>delete</button>
+                <button onClick={() => changeText(content.id)}>rewrite</button>
+              </div>
+            );
+          }
         })}
       </ul>
     </>
   );
 };
-
 export default List;
